4d4ae065ea77191ba5fcdbeb268dc5ed8085455d
Mostafa Asgari <mostafa.asg@gmail.com>
2017-04-15 14:46:25 +0430

    main function
    Sample usage

--- src/com/github/Program.kt
+++ src/com/github/Program.kt
@@ -1,5 +1,7 @@
 package com.github
 
+import java.util.*
+
 /**
  *
  * @author Mostafa Asgari
@@ -8,6 +10,29 @@ package com.github
 
 fun main(args : Array<String> ){
 
+    if( args.size > 0 ) {
+        val start = System.currentTimeMillis()
+
+        val spellChecker = SpellChecker.Builder.loadFromFile(args[0]).build()
+        println("loaded ${spellChecker.totalWords} words in ${(System.currentTimeMillis() - start) / 1000.0} seconds")
+
+        val scanner : Scanner = Scanner(System.`in`)
+
+        while (true){
+
+            println("enter your misspell word (enter q for exit) :")
+            val word = scanner.nextLine()
+            if( word==null || word.equals("q") )
+                break
+
+            val suggestion = spellChecker.suggest(word,1)
+            if (suggestion.size == 0)
+                println("it spelt correctly")
+            else
+                println(suggestion)
+        }
+
+    }
 
 }
 
